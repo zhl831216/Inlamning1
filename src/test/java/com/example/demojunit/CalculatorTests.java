32a5aec0f85e55ed1a7e00109c6398cca406e126
zhl831216 <zheng1216@gmail.com>
2023-04-19 08:11:07 +0200

    ok3

--- src/test/java/com/example/demojunit/CalculatorTests.java
+++ src/test/java/com/example/demojunit/CalculatorTests.java
@@ -18,7 +18,7 @@ public class CalculatorTests {
 
         int result = c.add(number1,number2);
         //then
-        Assertions.assertEquals(3,result);
+        Assertions.assertEquals(5,result);
     }
     //Testfall 2
     @Test
